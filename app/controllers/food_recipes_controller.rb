4fb24b398127092f2d79dfbb1bb4e568a77fd9bb
Nyaz Jaff <103050594+Jnyaz@users.noreply.github.com>
2022-05-23 09:28:39 +0100

    Add bootstrap

--- app/controllers/food_recipes_controller.rb
+++ app/controllers/food_recipes_controller.rb
@@ -6,7 +6,7 @@ class FoodRecipesController < ApplicationController
   def index
     @recipes = FoodRecipe.all
     if params['search_by_ingredient']
-      @recipes = Recipe::Ingredient.find_by_ingredients(params['search_by_ingredient'])
+      @recipes = Recipe::Ingredient.find_by_ingredients(params['search_by_ingredient'].gsub("'", ''))
     end
 
     @recipes = @recipes.paginate :page => params[:page], per_page: 25
